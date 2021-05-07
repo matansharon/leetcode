3775b13181282740f9e0dab1147c4fe5ce4b6e05
matansharon <49459070+matansharon@users.noreply.github.com>
2021-05-07 21:38:31 +0300

    Create Kids_With_the_Greatest_Number_of_Candies.py

---
+++ python/Kids_With_the_Greatest_Number_of_Candies.py
@@ -0,0 +1,17 @@
+def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        maxcandie=-1
+        res=[]
+        for i in candies:
+            if i>maxcandie:
+                maxcandie=i
+        for i in candies:
+            if i+extraCandies>=maxcandie:
+                res.append(True)
+            else:
+                res.append(False)
+        return res
