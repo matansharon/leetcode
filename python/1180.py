fcac910cb18caea498d696741a7df03ac223eb05
matansharon <49459070+matansharon@users.noreply.github.com>
2021-08-08 05:09:07 +0300

    Update 1180.py

--- python/1180.py
+++ python/1180.py
@@ -1,3 +1,22 @@
+# Given a string s, return the number of substrings that have only one distinct letter.
+
+ 
+
+# Example 1:
+
+# Input: s = "aaaba"
+# Output: 8
+# Explanation: The substrings with one distinct letter are "aaa", "aa", "a", "b".
+# "aaa" occurs 1 time.
+# "aa" occurs 2 times.
+# "a" occurs 4 times.
+# "b" occurs 1 time.
+# So the answer is 1 + 2 + 4 + 1 = 8.
+# Example 2:
+
+# Input: s = "aaaaaaaaaa"
+# Output: 55
+
 class Solution(object):
     def my_fac(self,val):
         res=0
