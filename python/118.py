d00b9bb86d3fb11707abd49ad3c1ab34e0db0f8e
matansharon <49459070+matansharon@users.noreply.github.com>
2021-09-13 12:30:26 +0300

    Update 118.py

--- python/118.py
+++ python/118.py
@@ -13,3 +13,26 @@
 
 # Input: numRows = 1
 # Output: [[1]]
+
+
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows==1:
+            return [[1]]
+        res=[[1],[1,1]]
+        for i in range(3,numRows+1):
+            res.append([0]*(i))
+        for i in range(1,len(res)-1):
+            prev=res[i]
+            curr=res[i+1]
+            for j in range(0,len(curr)):
+                if j==0 or j==len(curr)-1:
+                    curr[j]=1
+                else:
+                    curr[j]=prev[j-1]+prev[j]
+        return res
