1f8633ee56ce21da10e21920814d39d0761b134d
matansharon <49459070+matansharon@users.noreply.github.com>
2021-09-27 22:03:29 +0300

    Create 1952.py

---
+++ python/1952.py
@@ -0,0 +1,35 @@
+# Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
+
+# An integer m is a divisor of n if there exists an integer k such that n = k * m.
+
+ 
+
+# Example 1:
+
+# Input: n = 2
+# Output: false
+# Explantion: 2 has only two divisors: 1 and 2.
+# Example 2:
+
+# Input: n = 4
+# Output: true
+# Explantion: 4 has three divisors: 1, 2, and 4.
+
+
+class Solution(object):
+    def isThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        res=0
+        for i in range(1,n+1):
+            if n%i==0:
+                # print i
+                res+=1
+                if res>3:
+                    return False
+        if res==3:
+            return True
+        return False
+        
