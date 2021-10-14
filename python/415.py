95544374da268ea85faeb8c281893fa61e9e84b5
matansharon <49459070+matansharon@users.noreply.github.com>
2021-10-14 23:21:50 +0300

    Create 415.py

---
+++ python/415.py
@@ -0,0 +1,45 @@
+# Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+# You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+ 
+
+# Example 1:
+
+# Input: num1 = "11", num2 = "123"
+# Output: "134"
+# Example 2:
+
+# Input: num1 = "456", num2 = "77"
+# Output: "533"
+# Example 3:
+
+# Input: num1 = "0", num2 = "0"
+# Output: "0"
+ 
+
+# Constraints:
+
+# 1 <= num1.length, num2.length <= 104
+# num1 and num2 consist of only digits.
+# num1 and num2 don't have any leading zeros except for the zero itself.
+
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        n1=0
+        n2=0
+        p=0
+        for i in reversed(num1):
+            n1+=pow(10,p)*int(i)
+            p+=1
+        p=0
+        for i in reversed(num2):
+            n2+=pow(10,p)*int(i)
+            p+=1 
+        return str(n1+n2)
+        
