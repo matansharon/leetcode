dd202b751393f2a8109c3a6d5d7fc3731746ff00
matansharon <49459070+matansharon@users.noreply.github.com>
2021-12-10 06:40:44 +0200

    Create 219.py

---
+++ python/219.py
@@ -0,0 +1,45 @@
+# Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1], k = 3
+# Output: true
+# Example 2:
+
+# Input: nums = [1,0,1,1], k = 1
+# Output: true
+# Example 3:
+
+# Input: nums = [1,2,3,1,2,3], k = 2
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# 0 <= k <= 105
+
+
+class Solution(object):
+
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        dic={}
+        for i in range(len(nums)):
+            if nums[i] not in dic:
+                dic[nums[i]]=i
+            else:
+                if abs(dic[nums[i]]-i)<=k:
+                    return True
+                else:
+                    dic[nums[i]]=i
+        return False
+        
+        
